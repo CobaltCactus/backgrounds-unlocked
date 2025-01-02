ddfe189c86728937b8b30f6e68ace05e7ee83c86
CobaltCactus <ao3.cobaltcactus@gmail.com>
2025-01-01 19:55:47 -0600

    Backup at Wed Jan  1 19:55:47 CST 2025

--- backup.bash
+++ backup.bash
@@ -8,7 +8,7 @@ set -e;
 # MOD_SUBDIR_NAME="CircleOfTheSpores_db7e15fd-b2fc-b159-4bbd-1baab34d8c3a"
 # Look in "C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Projects"
 # for names of mods you have already started
-MOD_SUBDIR_NAME=""
+MOD_SUBDIR_NAME="backgrounds_unlocked_e3d00bd4-d7f0-acce-2417-545c427066ae"
 
 # This is the MinGW64 path to a Steam install of the toolkit
 BG3_DATA="/d/Program Files (x86)/Steam/steamapps/common/Baldurs Gate 3/Data"
